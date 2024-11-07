d20ead4875516f8c91b4726c9c55c1d970b5dbad
Mega.D <phamanhduc30012005@gmail.com>
2024-11-07 21:39:20 +0700

    Resolve merge conflict

---
+++ W1/Test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="Test.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Page not Found</title>
+</head>
+<body>   
+    <p>Welcome to <br><b><i>Spotify</i></b></p>
+    <hr>
+    <h1>#Thực đơn</h1>
+    <hr>
+    <ol type = "1">
+        <li>Beef Nam Bắc 2013</li>
+        <li>Beef Nam Bắc 2018</li>
+        <li>Rácpital</li>
+        <li>24cm.Right</li>
+        <li>Bò Jason</li>
+        <li>Gỗ </li>
+        <li>Nơi tình yêu bốk đầu</li>
+        <li>Tlinh Thộn</li>
+        <li>Chìm loi - MCK</li>
+    </ol>
+    <hr>
+    <p span="Text">Chúc quý khách ngon miệng trên và dưới</p>
+    <img src="./ATVNCG.png" alt="Test" width = 720px>
+    <link rel="stylesheet" href="facebook.com/megadiiiii">
+</body>
+</html>

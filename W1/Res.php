37a54e2d6c5957b1e77a3221e2841b104aa45e48
Mega.D <phamanhduc30012005@gmail.com>
2024-11-08 19:11:21 +0700

    Form

---
+++ W1/Res.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dang Ki</title>
+    <link rel="stylesheet" href="Res.css">
+</head>
+<body>
+    <div class="table" align="center">
+        <table>
+            <tr>
+                <th colspan = "2">Đăng kí du lịch</th>
+            </tr>
+
+            <tr>
+                <td>Họ và tên</td>
+                <td>
+                    <input class="Input" type="text" name="" id="">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Địa chỉ</td>
+                <td>
+                    <input class="Input" type="text" name="" id="">
+                </td>
+            </tr>
+
+            <tr>
+                <td>Điện thoại</td>
+                <td>
+                    <input class="Input" type="text" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>Khách Việt Nam</td>
+                <td>
+                    <input class="Input" type="checkbox" name="" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>Tour</td>
+                <td>
+                    <select class="Input" name="" id="">
+                        <h1>Miền Bắc</h1>
+                        <option value="">Hà Nội - Hạ Long - Sapa</option>
+                        <option value="">Hà Nội - Chùa Hương - Ninh Bình</option>
+                        <option value="">Hà Nội - Cát Bà - Tuần Châu</option>
+                        <option value="">Huế - Bạch Mã - Đà Nẵng</option>
+                        <option value="">Nha Trang - Đà Lạt</option>
+                        <option value="">Buôn Mê Thuột - Gia Lai - Kon Tum</option>
+                        <option value="">TP.HCM - Cần Thơ - Cà Mau</option>
+                        <option value="">TP.HCM - Mỹ Tho</option>
+                        
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Phương tiện</td>
+                <td>
+                    <input type="radio" name="Vehicle" id="">Máy bay
+                    <input type="radio" name="Vehicle" id="">Xe ô tô
+                </td>
+            </tr>
+            <tr>
+                <td>Ghi chú</td>
+                <td>
+                    <input class="Input" type="textarea" name="" id="" cols = "2">
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan = "2">
+                    <button type="submit">Đồng ý</button>
+                </td>
+            </tr>
+            <tr>
+                <td colspan = "2" span="Data">Bạn đã đăng kí thành công !!!</td>
+            </tr>
+
+        </table>
+    </div>
+</body>
+</html>

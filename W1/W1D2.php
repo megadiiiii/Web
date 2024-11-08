059b527a764765dbe6c927ec9af56c2dc1dbcdd7
Mega.D <phamanhduc30012005@gmail.com>
2024-11-08 13:55:42 +0700

    Edit CSS W1D2

--- W1/W1D2.php
+++ W1/W1D2.php
@@ -40,6 +40,55 @@
                 <td>3</td>
             </tr>
         </table>
+
+        <h1>Test</h1>
+        <table>
+            <tr>
+                <th rowspan = "2">Họ tên thí sinh</th>
+                <th rowspan = "2">Ngày sinh</th>
+                <th rowspan = "2">Giới tính</th>
+                <th colspan = "3">Kết quả thi</th>
+            </tr>
+
+            ><tr>
+                <th>Toán</th>
+                <th>Lí</th>
+                <th>Hoá</th>
+            </tr>
+
+            <tr>
+                <td>Nothing</td>
+                <td>27/03/2077</td>
+                <td>Flex</td>
+                <td>22</td>
+                <td>22</td>
+                <td>22</td>
+            </tr>
+            <tr>
+                <td>Nothing</td>
+                <td>27/03/2077</td>
+                <td>Flex</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+            </tr>
+            <tr>
+                <td>Nothing</td>
+                <td>27/03/2077</td>
+                <td>Flex</td>
+                <td>22</td>
+                <td>22</td>
+                <td>22</td>
+            </tr>
+            <tr>
+                <td>Nothing</td>
+                <td>27/03/2077</td>
+                <td>Flex</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>

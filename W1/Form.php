a93259f4dfb40cc2cb108975978a666103240520
Mega.D <phamanhduc30012005@gmail.com>
2024-11-08 14:57:52 +0700

    Form

--- W1/Form.php
+++ W1/Form.php
@@ -9,16 +9,28 @@
 <body>
     <div class="Text" align="center">
         <form action="" method="get">
-            <label class="Label" for="">Họ và tên</label>
+            <label class="Label" for="">Họ và tên</label> <br>
             <input type="text" name="Name"placeholder="Test"> <br>
-            <label class="Label" for="">Mật khẩu</label>
+            <label class="Label" for="">Mật khẩu</label> <br>
             <input type="password" name=txtPass placeholder="Test"> <br>
-            <label class="Label" for="">Ngày sinh</label>
+            <label class="Label" for="">Ngày sinh</label> <br>
             <input type="date" name=txtDate placeholder="Test"><br>
-            <label class="Label" for="">Số lượng</label>
+            <label class="Label" for="">Số lượng</label> <br>
             <input type="number" max = 10><br>
             <button type="submit">Submit</button>
             <button type="reset">Reset</button>
+            <br>
+            <label class="Label" for="">Phép toán</label> <br>
+            <input type="checkbox" name="" id="" >Cộng
+            <input type="checkbox" name="" id="" >Trừ 
+            <input type="checkbox" name="" id="" >Nhân
+            <input type="checkbox" name="" id="" >Chia <br>
+
+            <label class="Label" for="">Phép toán</label> <br>
+            <input type="radio" name="" id="" >Cộng
+            <input type="checkbox" name="" id="" >Trừ 
+            <input type="checkbox" name="" id="" >Nhân
+            <input type="checkbox" name="" id="" >Chia
         </form>
     </div>
 

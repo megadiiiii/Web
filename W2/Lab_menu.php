f7f8ba387117afd4d9dd3fa59b262e099f6aa1bc
Mega.D <phamanhduc30012005@gmail.com>
2024-11-15 01:13:15 +0700

    Level Menu

---
+++ W2/Lab_menu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Level Menu</title>
+    <link rel="stylesheet" href="Lab_menu.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <nav class="container">
+                <a class="logo" href="" id="">
+                    <img src="logo.png" alt="ATVNCG" width="150px">
+                </a>
+            <ul class="main">
+                <li><a href="">About</a></li>
+                <li><a href="">Sample</a>
+                    <ul class="sub">
+                        <li><a href="">Drop 1.1</a></li>
+                        <li><a href="">Drop 1.2</a></li>
+                        <li><a href="">Drop 1.3</a></li>
+                        <li><a href="">Drop 1.4</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Soundbank</a>
+                <ul class="sub">
+                        <li><a href="">Drop 2.1</a></li>
+                        <li><a href="">Drop 2.2</a></li>
+                        <li><a href="">Drop 2.3</a></li>
+                        <li><a href="">Drop 2.4</a></li>
+                    </ul>
+                </li>
+                <li><a href="">VSTs</a></li>
+                <li><a href="">Project</a></li>
+            </ul>
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
--- W2/W2_P48.php
+++ W2/W2_P48.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>W2 - P48</title>
-    
+    <link rel="stylesheet" href="W2_P48.css">
 </head>
 <body>
-    
+    <div class="header">
+        <img class="Logo" src="./logo.png" alt="">
+        <h1>#HNĐCMM</h1>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="./W2_P48.css">Link 1</a>
+                <ul>
+                    <li>Drop 1</li>                    
+                    <li>Drop 2</li>                    
+                    <li>Drop 3</li>                    
+                    <li>Drop 4</li>                    
+                </ul>
+            </li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li>
+            <li><a href="">Link 4</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <div class="left_menu">
+            <h1>Main</h1>
+            <p>nfuiewdfk</p>
+        </div>
+        <div class="body">
+            <h1>Main</h1>
+            <p>nfuiewdfkưdnvjdfvbkdbcuj</p>
+            
+        </div>
+    </div>
+    <div class="footer"></div>
 </body>
 </html>
